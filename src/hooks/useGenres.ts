819280ba965c2db211bace4dd12c40044c9a4c2f
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-23 00:16:57 +0800

    Create lookup hooks and refactor initial data.

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -16,6 +16,7 @@ const useGenres = () =>
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
     initialData: { count: genres.length, results: genres, next: null },
+    // initialData: genres,
   });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,8 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: platforms.length, results: platforms, next: null },
+    initialData: platforms,
+    // initialData: { count: platforms.length, results: platforms, next: null },
   });
 
 export default usePlatforms;
