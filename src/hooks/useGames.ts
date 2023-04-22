1e39d96a69a322517f9bcf373a3129850769e3f2
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-22 18:37:36 +0800

    Refactor: remove duplicate Platform interface.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,14 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "./usePlatforms";
 
 const apiClient = new APIClient<Game>("/games");
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 export interface Game {
   id: number;
@@ -26,7 +21,7 @@ const useGames = (gameQuery: GameQuery) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
-          platforms: gameQuery.platform?.id,
+          parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
         },
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import APIClient, { FetchResponse } from "../services/api-client";
 
-interface Platform {
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
+
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery<FetchResponse<Platform>, Error>({
+    queryKey: ["platforms"],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
