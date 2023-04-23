f3818b6754dc8ab0a8dc1530bc34e06ab3b8facd
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-23 21:58:33 +0800

    Manage state with zustand.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,15 +1,12 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatform from "../hooks/usePlatform";
+import usePlatforms from "../hooks/usePlatforms";
+import useGameQueryStore from "../store";
 
-import usePlatforms, { Platform } from "../hooks/usePlatforms";
-
-interface Props {
-  onSelectPlatform: (platform: Platform) => void;
-  selectedPlatformId: number | undefined;
-}
-
-const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
+const PlatformSelector = () => {
+  const setSelectedPlatformId = useGameQueryStore((s) => s.setPlatformId);
+  const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
   const { data, error } = usePlatforms();
 
   const selectedPlatform = usePlatform(selectedPlatformId);
@@ -24,7 +21,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
       <MenuList>
         {data?.results.map((platform) => (
           <MenuItem
-            onClick={() => onSelectPlatform(platform)}
+            onClick={() => setSelectedPlatformId(platform.id)}
             key={platform.id}
           >
             {platform.name}
