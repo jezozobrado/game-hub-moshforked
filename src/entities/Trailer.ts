0e71bd197e66109bc2902a8160442db78323b415
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-24 22:41:54 +0800

    Revert "Build game trailer."
    This reverts commit 6a7ee0688a7cef81e86c3b7612d8b28a87fb437f.

--- src/entities/Trailer.ts
+++
@@ -1,6 +0,0 @@
-export interface Trailer {
-  id: number;
-  name: string;
-  preview: string;
-  data: { 480: string; max: string };
-}
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,12 +1,7 @@
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
-import CriticScore from "../components/CriticScore";
-import DefinitionItem from "../components/DefinitionItem";
 import ExpandableText from "../components/ExpandableText";
-import GameAttributes from "../components/GameAttributes";
-import GameTrailer from "../components/GameTrailer";
-
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -21,8 +16,6 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
     </>
   );
 };
