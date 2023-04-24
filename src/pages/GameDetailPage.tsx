8071e7e5d0ce2dd2e0979cf13a54bd87495274b3
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-24 22:58:58 +0800

    Implement video trailer alone.

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { Heading, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
+import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -11,11 +12,11 @@ const GameDetailPage = () => {
 
   if (isLoading) return <Spinner />;
   if (error || !game) throw error;
-
   return (
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
